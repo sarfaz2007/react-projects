e007e57db566fe56e7352972ca6395ff54bbd5a4
sarfaz <sarfazsarfaz646@gmail.com>
2025-08-22 16:00:40 +0530

    22-8-2025

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,15 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-      <section className='h-[700px] w-[100%]'>
-        <div className='bg-blue-400 h-[700px] w-auto'>
-          <div className='relative right-0 z-10'>
-            <div className=' h-[500px] flex absolute top-0 right-0'>
-              <img src="src/assets/5313.jpg" alt="" />
-              <p className='bg-amber-500'>#HIKING</p>
-            </div>
+      <section className='h-[700px] w-[100%] flex relative'>
+        <div className='bg-blue-400 h-[700px] w-1/2'></div>
+        <div className='bg-green-400 h-[700px] w-1/2'></div>
+        <div className='w-full h-full flex-1 absolute flex justify-center items-center'>
+          <div className='h-[500px] flex'>
+            <img src="src/assets/5313.jpg" alt="" />
+            <p className='bg-amber-500'>#HIKING</p>
           </div>
         </div>
-
       </section>
     </>
   )
