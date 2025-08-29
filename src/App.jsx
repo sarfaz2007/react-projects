802a3248c224a20f3584cd75466bb8a5c2664a18
sarfaz <sarfazsarfaz646@gmail.com>
2025-08-29 13:29:40 +0530

    29/8/25

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/Home'
 import { Route, Routes } from 'react-router-dom'
 import Page1 from './pages/Page1'
 import Page2 from './pages/Page2'
+import Page3 from './pages/Page3'
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/page1" element={<Page1 />} />
         <Route path="/page2" element={<Page2 />} />
+        <Route path="/page3" element={<Page3 />} />
       </Routes>
      <Footer/>
 
--- src/pages/Page3.jsx
+++ src/pages/Page3.jsx
@@ -3,11 +3,20 @@ import React from 'react'
 function Page3() {
   return (
     <>
-    <section>
-        <h1>jmhnvgfngyfghbgvtfhctbdgfg</h1>
-    </section>
+      <section className='relative'>
+        <img src="src/assets/dss-min.jpg" alt="" className='w-full h-[800px] object-cover' />
+        <div className='absolute top-64    left-[600px] text-center text-white'>
+          <p className='text-9xl font-bold '>404</p>
+          <p className='text-3xl font-semibold pt-4'>SORRY PAGE NOT FOUND</p>
+          <p className='text-3xl pt-7'>The page you are looking for was moved, <br />removed, renamed or might never existed
+          </p>
+          <div className='border-2 h-14 w-48 items-center mt-7 ml-40'>
+            <p className='font-semibold p-3 font-stretch-50%'>GO TO <a href="/">HOMEPAGE</a></p>
+          </div>
+        </div>
+      </section>
     </>
-    
+
   )
 }
 
