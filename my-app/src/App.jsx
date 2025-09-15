a68e46952cefb865480452648f8ed4dc4037400b
sarfaz <sarfazsarfaz646@gmail.com>
2025-09-15 13:54:26 +0530

    15/9/25

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './assets/components/Navbar'
+import "@fortawesome/fontawesome-free/css/all.min.css"
+
 
 function App() {
   const [count, setCount] = useState(0)
--- my-app/src/assets/components/Navbar.jsx
+++ my-app/src/assets/components/Navbar.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CiMenuBurger } from "react-icons/ci";
 
-function Navbar() {
+function Navbar(){
+    const [nav,setNav] = useState(false)
+
+    const handleNav = () => {
+      setNav(!nav)
+    }
+
   return (
     <>
 
@@ -13,6 +20,19 @@ function Navbar() {
           <li className='p-4'>About</li>
           <li className='p-4'>Contact</li>
         </ul>
+        <div>
+          <CiMenuBurger size={20} />
+        </div>
+        <div className='fixed left-0 top-0 h-full w-[60%] border-r border-r-gray-900 bg-[#000300]'>
+           <h1 className='w-full text-3xl font-bold m-4 text-[#00df9a]'>REACT.</h1>
+          <ul className='uppercase p-4'>
+            <li className='p-4 border-b border-gray-600'>Home</li>
+            <li className='p-4 border-b border-gray-600'>Company</li>
+            <li className='p-4 border-b border-gray-600'>Resources</li>
+            <li className='p-4 border-b border-gray-600'>About</li>
+            <li className='p-4'>Contact</li>
+          </ul>
+        </div>
       </div>
     </>
 
