ced6db113b261921163bc42349c83b1b3ca20406
sarfaz <sarfazsarfaz646@gmail.com>
2025-09-18 13:46:16 +0530

    18/9/25

--- my-app/src/assets/components/Hero.jsx
+++ my-app/src/assets/components/Hero.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { ReactTyped } from "react-typed"
+
 
 export const Hero = () => {
   return (
     <div className='text-white'>
-        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center '>
+      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center '>
+        <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALALYTICS</p>
+        <h1 className='md:text-7xl sm:text-7xl text-4xl font-bold md:py-6'>Grow with data</h1>
+        <div>
+          <p className='md:text-5xl sm:text-4xl text-xl'>Fast, flexible financing for
 
+          </p>
+          <ReactTyped
+          className="md:text-5xl sm:text-4xl text-xl font-bold content-center"
+            strings={[
+              "BTB",
+              "BTC",
+              "SASS",
+            ]}
+            typeSpeed={40}
+            backSpeed={50}
+            attr="placeholder"
+            loop
+          >
+            <input type="text" />
+          </ReactTyped>
         </div>
+      </div>
     </div>
   )
 }
